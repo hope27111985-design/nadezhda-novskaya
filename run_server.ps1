81822044992de5f4060aca09ec25ba4b0837d3e8
hope27111985-design <hope27111985@gmail.com>
2026-05-21 01:08:45 +0300

    Add run_server.ps1

---
+++ run_server.ps1
@@ -0,0 +1,27 @@
+﻿$url = 'http://localhost:8081/'
+$root = 'C:\Users\MSI-\Desktop\Сеул 2026'
+$http = New-Object System.Net.HttpListener
+$http.Prefixes.Add($url)
+$http.Start()
+$done = $false
+$mime = @{'.html'='text/html; charset=utf-8';'.css'='text/css';'.js'='application/javascript';'.png'='image/png';'.jpg'='image/jpeg';'.jpeg'='image/jpeg'}
+while (-not $done) {
+    $task = $http.GetContextAsync()
+    $wait = $task.Wait(300000)
+    if (-not $wait) { continue }
+    $ctx = $task.Result
+    $path = $ctx.Request.Url.LocalPath.TrimStart('/')
+    $fullPath = [IO.Path]::Combine($root, $path)
+    if (Test-Path -LiteralPath $fullPath -PathType Leaf) {
+        $bytes = [IO.File]::ReadAllBytes($fullPath)
+        $ext = [IO.Path]::GetExtension($fullPath).ToLower()
+        if ($mime.ContainsKey($ext)) { $ctx.Response.ContentType = $mime[$ext] }
+        $ctx.Response.OutputStream.Write($bytes, 0, $bytes.Length)
+    } else {
+        $idx = [IO.Path]::Combine($root, 'index.html')
+        $bytes = [IO.File]::ReadAllBytes($idx)
+        $ctx.Response.ContentType = 'text/html; charset=utf-8'
+        $ctx.Response.OutputStream.Write($bytes, 0, $bytes.Length)
+    }
+    $ctx.Response.Close()
+}
